1f893e02fb39beb75ac6c58c4907825a1935db8f
willyantoteguh <willyantoteguh1@gmail.com>
2020-11-26 06:51:00 +0700

    Project started

---
+++ app/Views/layout/sidebar.php
@@ -0,0 +1,18 @@
+<aside class="main-sidebar">
+    <section class="sidebar">
+        <div class="user-panel">
+            <ul class="sidebar-menu">
+                <div class="pull-left image">
+                    <img src="<?= base_url(); ?>/images/user.png" class="img-circle" alt="Foto Profil" />
+                </div>
+                <div class="pull-left info">
+                    <p>Food dev</p>
+                    <a href="<?= site_url('#'); ?>">
+                        <i class="fa fa-circle text-success"></i>
+                        Online
+                    </a>
+                </div>
+            </ul>
+        </div>
+    </section>
+</aside>
