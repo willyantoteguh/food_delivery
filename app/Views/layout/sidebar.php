edd7696df818f1792f8cdda0e86b0066e7f925cb
willyantoteguh <willyantoteguh1@gmail.com>
2020-11-26 07:04:17 +0700

    Sidebar Menu

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -1,18 +1,40 @@
 <aside class="main-sidebar">
     <section class="sidebar">
         <div class="user-panel">
-            <ul class="sidebar-menu">
                 <div class="pull-left image">
                     <img src="<?= base_url(); ?>/images/user.png" class="img-circle" alt="Foto Profil" />
                 </div>
                 <div class="pull-left info">
-                    <p>Food dev</p>
+                    <p>Food Delivery</p>
                     <a href="<?= site_url('#'); ?>">
                         <i class="fa fa-circle text-success"></i>
                         Online
                     </a>
                 </div>
-            </ul>
+                <ul class="sidebar-menu">
+                    <li class="header">MAIN NAVIGATION</li>
+
+                    <li class="<?= ($halaman == 'dashboard') ? 'active' : ''; ?>">
+                        <a href="<?= site_url('dashboard');  ?>">
+                            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="<?= ($halaman == 'dashboard') ? 'active' : ''; ?>">
+                        <a href="<?= site_url('dashboard');  ?>">
+                            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="<?= ($halaman == 'dashboard') ? 'active' : ''; ?>">
+                        <a href="<?= site_url('dashboard');  ?>">
+                            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="<?= ($halaman == 'dashboard') ? 'active' : ''; ?>">
+                        <a href="<?= site_url('dashboard');  ?>">
+                            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                        </a>
+                    </li>
+                </ul>
         </div>
     </section>
 </aside>
